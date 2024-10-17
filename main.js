576e9079be614243535561d29688401a3fd9c6e4
Aditya Raj Arora <nathanarsement@gmail.com>
2024-10-17 22:13:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var students = {
+    name:'Akshita',
+    age:'13',
+    grade:'8',
+    hobbies:'Drawing',
+    fav_sport:'Basketball'
+};
+console.log("Student name is", students.name,"her fav sport is", students.fav_sport)
